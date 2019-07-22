39c42b9c11c28d938bf12589416776b772494193
Yuta Onuki <onu4648yu@gmail.com>
2019-07-22 23:54:17 +0900

    feat: header表示実装
    vue.js単一コンポーネントでheaderが表示されるように実装
    
    一緒にコミットしたhello_vue.jsは前回コミットの時の追加漏れ
    
    hello_vue.jsは今回の変更に直接の関係はない

---
+++ app/javascript/packs/components/header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul id="dropdown" class="dropdown-content">
+      <li><a href="#"></a>Top</li>
+      <li><a href="#"></a>About</li>
+      <li><a href="#"></a>Content</li>
+    </ul>
+    <nav>
+      <div class="nav-wrapper container">
+        <a href="/" class="brand-log left">ToDo Application</a>
+        <ul class="right hide-on-large-only">
+          <li>
+            <a class="dropdown-button" href="#!" data-activates="dropdwon">
+              MENU<i class="material-icons right">arrow_drop_down</i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
