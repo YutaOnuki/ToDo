8c45e463507acf7591d34dec6d25cf547640a9bd
Yuta Onuki <onu4648yu@gmail.com>
2019-07-23 22:22:09 +0900

    feat: 各Vueファイルを作成

---
+++ app/javascript/packs/components/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p>Ruby On Rails + Vue.js + Bulmaを使ったToDoリストアプリです。</p>
+    <p>作成したコードは→<a href="https://github.com/YutaOnuki/ToDo" target="_blank">です。</p>
+  </div>
+</template>
---
+++ app/javascript/packs/components/contact.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p>このアプリをみてコンタクトを撮りたい時は、下記のメールアドレスまで連絡ください。</p>
+    <p>sample@email.com</p>
+  </div>
+</template>
