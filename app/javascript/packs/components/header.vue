0a51a7d9cd8fff911180643499f4cdc1a9b3a15f
Yuta Onuki <onu4648yu@gmail.com>
2019-07-23 21:57:51 +0900

    feat: ヘッダーsassの適用
    bulmaのコンポーネントを利用して、ヘッダーのレイアウトを整える

--- app/javascript/packs/components/header.vue
+++ app/javascript/packs/components/header.vue
@@ -1,21 +1,28 @@
 <template>
-  <div>
-    <ul id="dropdown" class="dropdown-content">
-      <li><a href="#"></a>Top</li>
-      <li><a href="#"></a>About</li>
-      <li><a href="#"></a>Content</li>
-    </ul>
-    <nav>
-      <div class="nav-wrapper container">
-        <a href="/" class="brand-log left">ToDo Application</a>
-        <ul class="right hide-on-large-only">
-          <li>
-            <a class="dropdown-button" href="#!" data-activates="dropdwon">
-              MENU<i class="material-icons right">arrow_drop_down</i>
-            </a>
-          </li>
-        </ul>
+  <nav class="navbar is-info" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">ToDo Application</a>
+      <a role="button" class="navbar-burger is-active" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start"></div>
+      <div class="navbar-end">
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            More
+          </a>
+          <div class="navbar-dropdown">
+            <a class="navbar-item" href="#">Top</a>
+            <a class="navbar-item" href="#">About</a>
+            <a class="navbar-item" href="#">Content</a>
+          </div>
+        </div>
       </div>
-    </nav>
-  </div>
+    </div>
+  </nav>
 </template>
